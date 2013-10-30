6e296088db12c7bddd02c40c45385967c0506cbf
Connor Glosser <glosser1@gmail.com>
2013-10-30 18:17:52 -0400

    Implement feq collision

--- src/lbm.cpp
+++ src/lbm.cpp
@@ -1,4 +1,5 @@
 #include <boost/multi_array.hpp>
+#include <Eigen/Dense>
 #include <iostream>
 #include <math.h>
 #include <vector>
@@ -11,27 +12,36 @@ typedef boost::multi_array<double, 2> Lattice2D;
 void streamingUpdate(Lattice3D &, const int, const int, const int);
 void collisionUpdate(const int, const int, const int, const vector<double> &,
     Lattice3D &);
-void dirToSteps(const int, int &, int &);
+Eigen::Vector2d dirToSteps(const int);
 void printLattice(const Lattice3D &, const int, const int, const int);
+double latticeDistance(int);
+int mod(int, int);
 
 int main() {
   // Define weightings for a D2Q9 lattice.
-  double weights[] = {4.0/9,  1.0/9,  1.0/9, 
-                      1.0/9,  1.0/9,  1.0/36, 
-                      1.0/36, 1.0/36, 1.0/36}; 
-
-  const int XDIM = 3, YDIM = 3, NUM_WEIGHTS = 9;
+  double weights[] = {4.0/9, 1.0/9, 1.0/36,
+                     1.0/9, 1.0/36, 1.0/9,
+                      1.0/36, 1.0/9, 1.0/36};
+  const int XDIM = 5, YDIM = 5, NUM_WEIGHTS = 9;
   const vector<double> weight(weights, weights + NUM_WEIGHTS);
   Lattice3D lboltz(boost::extents[XDIM][YDIM][NUM_WEIGHTS]);
 
   for(int y = YDIM - 1; y >= 0; y--) {
     for(int x = 0; x < XDIM; x++) {
       for(int w = 0; w < NUM_WEIGHTS; w++) {
-        if(x == 1 && y == 1) {
-          lboltz[x][y][w] = 0;
-        }
+        lboltz[x][y][w] = 0;
       }
     }
+  }
+
+  lboltz[XDIM/2][YDIM/2][1] = 1;
+  printLattice(lboltz, XDIM, YDIM, 1);
+  cout << endl;
+
+  for(int t = 0; t < 10; t++) {
+    streamingUpdate(lboltz, XDIM, YDIM, NUM_WEIGHTS);
+    printLattice(lboltz, XDIM, YDIM, 1);
+    cout << endl;
   }
 
   return 0;
@@ -44,14 +54,13 @@ void streamingUpdate(Lattice3D &lb, const int XDIM, const int YDIM,
   for(int x = 0; x < XDIM; x++) {
     for(int y = 0; y < YDIM; y++) {
       for(int w = 0; w < NUM_WEIGHTS; w++) {
-        int dx = 0, dy = 0;
-        dirToSteps(w, dx, dy);
-        // Free boundary conditions, for the time being - particles just fall
+        Eigen::Vector2d dr(dirToSteps(w));
+        // Periodic boundary conditions, for the time being
         // off the edge
-        if ((x + dx) >= XDIM || (x + dx) < 0) continue;
-        if ((y + dy) >= YDIM || (y + dy) < 0) continue;
+        int xprime = x + int(dr[0]), yprime = y + int(dr[0]);
+        xprime = mod(xprime, XDIM); yprime = mod(yprime, YDIM);
 
-        temp_lattice[x + dx][y + dy][w] = lb[x][y][w];
+        temp_lattice[xprime][yprime][w] = lb[x][y][w];
       }
     }
   }
@@ -63,31 +72,29 @@ void streamingUpdate(Lattice3D &lb, const int XDIM, const int YDIM,
 
 void collisionUpdate(const int XDIM, const int YDIM, 
     const int NUM_WEIGHTS, const vector<double> &weight, Lattice3D &lb) {
-  Lattice2D density(boost::extents[XDIM][YDIM]);
-  Lattice3D equilibrium(boost::extents[XDIM][YDIM][NUM_WEIGHTS]);
+  const double m = 1, c = 1, tau = 1;
 
   for(int x = 0; x < XDIM; x++) {
     for(int y = 0; y < YDIM; y++) {
-      density[x][y] = 0;
+
+      Eigen::Vector2d macro_vel(0,0);
+      double density = 0;
       for(int w = 0; w < NUM_WEIGHTS; w++) {
-        density[x][y] += lb[x][y][w];
+        macro_vel += c*lb[x][y][w]*dirToSteps(w);
+        density += lb[x][y][w];
       }
-    }
-  }
+      if (density != 0) macro_vel /= density;
 
-  for(int x = 0; x < XDIM; x++) {
-    for(int y = 0; y < ydim; y++) {
       for(int w = 0; w < NUM_WEIGHTS; w++) {
-        double fi = lb[x][y][w], rho = density[x][y], u_dot_u = 0;
+        double equilibrium = 0;
 
-        for(int w = 0; w < NUM_WEIGHTS; w++) {
-          u_dot_u += pow(lb[x][y][w], 2)*pow(latticeDistance(w),2);
-        }
+        double e_dot_u = dirToSteps(w).dot(macro_vel);
+ 
+        equilibrium = 1 + 3/c*e_dot_u + 9/(2*pow(c,2))*pow(e_dot_u,2)
+            - 3/(2*pow(c,2))*macro_vel.dot(macro_vel);
+        equilibrium *= weight[w]*density;
 
-        equilibrium[x][y][w] = rho*weight[w](1 + 
-            3*fi/rho*pow(latticeDistance(w),2) +
-            9*pow(fi,2)/(2*pow(rho,2))*pow(latticeDistance(w),4) -
-            3/(2*pow(rho,2))*u_dot_u);
+        lb[x][y][w] = lb[x][y][w] - 1/tau*(lb[x][y][w] - equilibrium);
       }
     }
   }
@@ -104,34 +111,35 @@ void collisionUpdate(const int XDIM, const int YDIM,
  *
  * \return Nothing
  */
-void dirToSteps(const int w, int &dx, int &dy) {
+Eigen::Vector2d dirToSteps(const int w) {
   switch(w) {
+    default:
     case 0:
-      dx = 0; dy = 0;
+      return Eigen::Vector2d(0, 0);
       break;
     case 1:
-      dx = 1; dy = 0;
+      return Eigen::Vector2d(1, 0);
       break;
     case 2:
-      dx = 1; dy = 1;
+      return Eigen::Vector2d(1, 1);
       break;
     case 3:
-      dx = 0; dy = 1;
+      return Eigen::Vector2d(0, 1);
       break;
     case 4:
-      dx = -1; dy = 1;
+      return Eigen::Vector2d(-1, 1);
       break;
     case 5:
-      dx = -1; dy = 0;
+      return Eigen::Vector2d(-1, 0);
       break;
     case 6:
-      dx = -1; dy = -1;
+      return Eigen::Vector2d(-1, -1);
       break;
     case 7:
-      dx = 0; dy = -1;
+      return Eigen::Vector2d(0, -1);
       break;
     case 8:
-      dx = 1; dy = -1;
+      return Eigen::Vector2d(1, -1);
       break;
   }
 }
@@ -156,4 +164,9 @@ double latticeDistance(int direction) {
     return -1;
 }
 
-
+int mod(int a, int b) {
+  int ret = a % b;
+  if (ret < 0)
+    ret += b;
+  return ret;
+}
